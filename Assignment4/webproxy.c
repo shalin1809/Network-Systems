52c652e5d120a5d8daf64b6bf7ed25005a307c0a
Shalin Shah <shalin.d.shah@colorado.edu>
2017-12-06 17:56:42 -0700

    Cached websites being served. Timeout not implemented yet

--- Assignment4/webproxy.c
+++ Assignment4/webproxy.c
@@ -11,6 +11,7 @@
 #include<stdio.h>
 #include<stdint.h>
 #include<stdbool.h>
+#include<openssl/md5.h>
 
 #define CONNMAX 1000
 #define BYTES 1024
@@ -138,13 +139,13 @@ void respond(int conn){
     char mesg[1500], copy[1500], *reqline[3];
     char data_to_send[1500], data_received[1500];
     char website[1000], temp[512];
-    char * webptr, *req_ptr;
+    char * webptr, *req_ptr, *webaddr,*hash;
     char * sendbuf = data_to_send;
-    char DNStemp[100];
+    char DNStemp[100],md[MD5_DIGEST_LENGTH],hashedASCII[32];
     int rcvd;
     int offset;
     struct sockaddr_in sockDNS;
-    FILE *DNSfile;
+    FILE *DNSfile, *SITEfile;
 
     //Reset the message buffer
 	memset( (void*)mesg, (int)'\0', 1500);
@@ -177,7 +178,7 @@ void respond(int conn){
                 printf("Website does not contain www\n");
                 if((webptr = strstr(reqline[1],"://")) == NULL){    //If the website requested does not have ://
                     printf("Website does not contain ://\n");
-//TODO: Invalid website, Not found error
+//TODO: Reply Not found error
                 }
                 else{   //If the website has :// after http, then set webptr to after ://
                     webptr+=3;  //3 for ://
@@ -192,7 +193,7 @@ void respond(int conn){
 
             //Check if website is blocked
             if(checkInFile(website,"BlockedList")){
-                bzero(data_to_send,sizeof(data_to_send));
+siteblocked:    bzero(data_to_send,sizeof(data_to_send));
                 //Prepare the send buffer with the bad request header
                 offset = sprintf(sendbuf,"HTTP/1.1 403 Forbidden\n");
                 sendbuf+=offset;
@@ -224,8 +225,8 @@ void respond(int conn){
             hints.ai_family = AF_INET;  //To force IPv4
             hints.ai_socktype = SOCK_STREAM;
             //Check if the IP exists in the DNS cache
-            webptr = getvalue(website,"DNScache");
-            if(webptr == NULL){
+            webaddr = getvalue(website,"DNScache");
+            if(webaddr == NULL){
 
                 //Lookup DNS to get the IP of the website
                 if ((rv = getaddrinfo(website, "80", &hints, &servinfo)) != 0){
@@ -247,6 +248,13 @@ void respond(int conn){
                     }
                     //Copy the socket structure
                     memcpy(&sockDNS,p->ai_addr,sizeof(sockDNS));
+                    //Check if website is blocked
+                    if(checkInFile(inet_ntoa(sockDNS.sin_addr),"BlockedList")){
+                        close(soc);
+                        goto siteblocked;
+                        //iteblocked will send 403 Forbidden Error
+                    }
+
                     //prepare a temp buffer with the website name and IP address
                     sprintf(DNStemp,"%s %s\n",website,inet_ntoa(sockDNS.sin_addr));
                     //Open the file in append mode
@@ -269,10 +277,10 @@ void respond(int conn){
             }
             //Found the website in DNScache, use that to connect
             else{
-                printf("Website %s found in cache: %s\n",website,webptr);
+                printf("Website %s found in cache: %s\n",website,webaddr);
                 sockDNS.sin_family = AF_INET;
                 sockDNS.sin_port = htons(80);
-                sockDNS.sin_addr.s_addr = inet_addr(webptr);
+                sockDNS.sin_addr.s_addr = inet_addr(webaddr);
                 if((soc = socket(AF_INET, SOCK_STREAM,0)) == -1) {
                     perror("socket");
                 }
@@ -286,26 +294,55 @@ void respond(int conn){
                 }
             }
 
-
-            //Send the GET request to the website
-            send(soc, copy, rcvd,0);
-            //Receive the data from the website
-            while((rcvd = recv(soc, data_received, sizeof(data_received), 0))){
-                printf("\n\nData bytes %d and message received from website:\n%s\n",rcvd,data_received);
-//TODO: Add site caching code here
-                //Prepare the packet to be sent
-                bzero(data_to_send,sizeof(data_to_send));
-                // offset = sprintf(sendbuf,"%s 200 OK\n",reqline[2]);
-                // sendbuf+=offset;
-
-                send(clients[conn], data_received, rcvd,0);
+            hash =(char *)MD5((unsigned char*)webptr,strlen(webptr),(unsigned char*)md);
+            hash = hashedASCII;
+            for(int i =0; i<16;i++){
+                hash += sprintf(hash,"%0x",md[i]);
             }
-
-            //Close the socket used to connect to the website
-            close(soc);
+            printf("\nThe hash value for %s is %s \n",webptr,hashedASCII);
+            SITEfile = fopen(hashedASCII,"r");
+            if(SITEfile == NULL){   //Cached file does not exist
+                //Send the GET request to the website
+                send(soc, copy, rcvd,0);
+                //Delete the cached file if it exists
+                remove(hashedASCII);
+                //Create a new file with the website's URL as name
+                //TODO: hash the file name
+                SITEfile = fopen(hashedASCII,"w+");
+                //Receive the data from the website
+                while((rcvd = recv(soc, data_received, sizeof(data_received), 0))){
+                    printf("\n\nData bytes %d and message received from website:\n%s\n",rcvd,data_received);
+                    //Write the received data to file
+                    fwrite(data_received,1,rcvd,SITEfile);
+                    //Send the received file without any modifications
+                    send(clients[conn], data_received, rcvd,0);
+                }
+                fclose(SITEfile);
+                //Close the socket used to connect to the website
+                close(soc);
+            }
+            else{   //Cached file exists
+//TODO: Check for cached file timeout here
+                int filesize = get_file_size(hashedASCII);
+                int packet, max_packets = filesize/1500;
+                printf("\n****************Cached file exists*************\n\n");
+                for(packet = 0 ;packet<=max_packets;packet++){
+                    //Prepare the packet to be sent
+                    bzero(data_to_send,sizeof(data_to_send));
+                    //Move the file pointer to the corresponding packet number
+                    fseek(SITEfile,packet*1500,SEEK_SET);
+                    //Load the file onto the send buffer
+                    fread(data_to_send,1500,1,SITEfile);
+                    printf("\n\n\n\nData from cache: \n%s\n",data_to_send);
+                    //Send the read data to the browser
+                    send(clients[conn], data_to_send, 1500,0);
+                }
+                fclose(SITEfile);
+                //Close the socket used to connect to the website
+                close(soc);
+            }
         }
-        else{
-//Send HTTP/1.1 400 Bad Request
+        else{//Send HTTP/1.1 400 Bad Request
             if((req_ptr = strchr(reqline[0],'\r'))!=NULL)   //If only the method is specified
                 strcpy(req_ptr,"\0");
             bzero(data_to_send,sizeof(data_to_send));
